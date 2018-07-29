af52dbfec8ecddb2d8b8f48a8c485f46e5631f59
Renzo R. Carpio Ponce <me@renzocarpio.com>
2018-07-29 00:31:46 -0500

    Validacion de propiedes tales como tipo, requerido y devolviendo un metodo en el html

--- src/TituloComponent.vue
+++ src/TituloComponent.vue
@@ -1,8 +1,22 @@
 <template>
-	<h2>{{ titulo }}</h2>
+	<div>
+		<h2>{{ tituloMayusculas() }}</h2>
+		<h3>Tareas por hacer:</h3>
+	</div>
 </template>
 <script>
 export default {
-	props:['titulo']
+	props:{
+		titulo:{
+			required:true,
+			type: String,
+			default: 'Tareas',
+		}
+	},
+	methods:{
+		tituloMayusculas(){
+			return this.titulo.toUpperCase();
+		}
+	}
 }	
 </script>
